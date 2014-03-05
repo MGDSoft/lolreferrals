d79082b22c7af7a376e0f42124a32f356dd79fe5
MiguelGD <miguelgd@mgdsoftware.es>
2014-03-05 04:58:52 +0100

    test dynamic parameters

---
+++ app/config/parameters_dynamic.php
@@ -0,0 +1,7 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+
+$container->setParameter('paypal_api_username', 'miguelgd1985_api1.mgdsoftware.es');
+$container->setParameter('paypal_api_password', '1393989564');
+$container->setParameter('paypal_api_signature', 'Ao4K48En.nrD.jKiISKaWvWhMzUsAtf2FL1SUxEzJ.HXDiEte19g7EmK');
