b434155bc18ea17d8966902b73a2280de2f74f53
MiguelGD <miguelgd@mgdsoftware.es>
2014-04-30 17:31:15 +0200

    Cache error 405 Method Not Allowed , Pending to solve...

--- web/app.php
+++ web/app.php
@@ -19,7 +19,7 @@ require_once __DIR__.'/../app/AppCache.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 // This line generate this error: 405 Method Not Allowed _PUT, _DELETE ...
-$kernel = new AppCache($kernel);
+// $kernel = new AppCache($kernel);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
