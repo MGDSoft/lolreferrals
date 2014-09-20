7fd286fdb43cb092da5cde9781b5f56333105086
MGDSoft <miguelgd@mgdsoftware.es>
2014-09-20 19:24:59 +0200

    fix set machine names

--- app/DoctrineMigrations/Version20140920190113.php
+++ app/DoctrineMigrations/Version20140920190113.php
@@ -16,6 +16,12 @@ class Version20140920190113 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
         $this->addSql("ALTER TABLE pedido_bots ADD maquina VARCHAR(255) DEFAULT NULL");
+        $this->addSql("
+            update pedido_bots
+            inner join REFSEU on REFSEU.pedido_bot_id = pedido_bots.id
+            set
+                pedido_bots.maquina = REFSEU.BOTID;
+        ");
     }
 
     public function down(Schema $schema)
